320b29a7a731c275f46bc032b4da03247e61b7ff
yuzunosk55 <yuzuha3ds@gmail.com>
2025-06-07 15:50:28 +0900

    add; lsp

--- nvim2/init.lua
+++ nvim2/init.lua
@@ -89,3 +89,115 @@ end)
 later(function()
   require('mini.surround').setup()
 end)
+
+-- キーマップ表示プラグイン
+later(function()
+  local function mode_nx(keys)
+    return { mode = 'n', keys = keys }, { mode = 'x', keys = keys }
+  end
+  local clue = require('mini.clue')
+  clue.setup({
+    triggers = {
+      -- Leader triggers
+      mode_nx('<leader>'),
+
+      -- Built-in completion
+      { mode = 'i', keys = '<c-x>' },
+
+      -- `g` key
+      mode_nx('g'),
+
+      -- Marks
+      mode_nx("'"),
+      mode_nx('`'),
+
+      -- Registers
+      mode_nx('"'),
+      { mode = 'i', keys = '<c-r>' },
+      { mode = 'c', keys = '<c-r>' },
+
+      -- Window commands
+      { mode = 'n', keys = '<c-w>' },
+
+      -- bracketed commands
+      { mode = 'n', keys = '[' },
+      { mode = 'n', keys = ']' },
+
+      -- `z` key
+      mode_nx('z'),
+
+      -- surround
+      mode_nx('s'),
+
+      -- text object
+      { mode = 'x', keys = 'i' },
+      { mode = 'x', keys = 'a' },
+      { mode = 'o', keys = 'i' },
+      { mode = 'o', keys = 'a' },
+
+      -- option toggle (mini.basics)
+      { mode = 'n', keys = 'm' },
+    },
+
+    clues = {
+      -- Enhance this by adding descriptions for <Leader> mapping groups
+      clue.gen_clues.builtin_completion(),
+      clue.gen_clues.g(),
+      clue.gen_clues.marks(),
+      clue.gen_clues.registers({ show_contents = true }),
+      clue.gen_clues.windows({ submode_resize = true, submode_move = true }),
+      clue.gen_clues.z(),
+    },
+  })
+end)
+
+-- lsp
+now(function()
+  vim.diagnostic.config({
+    virtual_text = true
+  })
+
+  create_autocmd('LspAttach', {
+    callback = function(args)
+      local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+
+      vim.keymap.set('n', 'grd', function()
+        vim.lsp.buf.definition()
+      end, { buffer = args.buf, desc = 'vim.lsp.buf.definition()' })
+
+      vim.keymap.set('n', '<space>i', function()
+        vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+      end, { buffer = args.buf, desc = 'Format buffer' })
+    end,
+  })
+
+  vim.lsp.config('*', {
+    root_markers = { '.git' },
+  })
+  vim.lsp.config('lua_ls', {
+    cmd = { 'lua-language-server' },
+    filetypes = { 'lua' },
+    on_init = function(client)
+      if client.workspace_folders then
+        local path = client.workspace_folders[1].name
+        if path ~= vim.fn.stdpath('config') and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then
+          return
+        end
+      end
+      client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+        runtime = { version = 'LuaJIT' },
+        workspace = {
+          checkThirdParty = false,
+          library = vim.list_extend(vim.api.nvim_get_runtime_file('lua', true), {
+            '${3rd}/luv/library',
+            '${3rd}/busted/library',
+          }),
+        }
+      })
+    end,
+    settings = {
+      Lua = {}
+    }
+  })
+  vim.lsp.enable('lua_ls')
+end)
